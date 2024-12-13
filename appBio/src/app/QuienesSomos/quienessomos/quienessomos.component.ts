cd55e1ce513249993f2fa07d92ecdde39515a9d2
MarianoOliva <nicolas.oliva.montivero@gmail.com>
2024-12-13 17:17:26 -0300

    Agregar Mariano a Quienes somos

--- appBio/src/app/QuienesSomos/quienessomos/quienessomos.component.ts
+++ appBio/src/app/QuienesSomos/quienessomos/quienessomos.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-quienessomos',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './quienessomos.component.html',
-  styleUrl: './quienessomos.component.css'
+  styleUrls: ['./quienessomos.component.css']
 })
 export class QuienessomosComponent {
-
+  integrantes = [
+    { nombre: 'Quimey', apellido: 'Apellido1' },
+    { nombre: 'Paz', apellido: 'Apellido2' },
+    { nombre: 'Roxana', apellido: 'Apellido3' },
+    { nombre: 'Mariano', apellido: 'Oliva' }  // Nuevo integrante
+  ];
 }
