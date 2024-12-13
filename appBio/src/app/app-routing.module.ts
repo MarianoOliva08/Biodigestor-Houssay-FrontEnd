1744208370a560ebb9ea62cea87fec2f137fa190
MarianoOliva <nicolas.oliva.montivero@gmail.com>
2024-12-12 22:47:21 -0300

    Creacion de componente forum

--- appBio/src/app/app-routing.module.ts
+++ appBio/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientesComponent } from './components/clientes/clientes.component';
-import { ForoComponent } from './foro/foro.component'; // Importa el ForoComponent correctamente
 
 const routes: Routes = [
   { path: 'clientes', component: ClientesComponent },
-  { path: 'foro', component: ForoComponent },
   { path: '', redirectTo: '/clientes', pathMatch: 'full' }
 ];
 
@@ -14,4 +12,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
